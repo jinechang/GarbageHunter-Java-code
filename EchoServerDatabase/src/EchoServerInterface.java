ea8cbe53c7c7da3caacda3ec47ba05039e401131
jinechang <chan0359@algonquinlive.com>
2016-03-12 07:55:05 +0800

    Remove wrong derectory

--- EchoServerDatabase/src/EchoServerInterface.java
+++
@@ -1,7 +0,0 @@
-import java.rmi.Remote;
-
-/** Remote interface for the RMI-based EchoServer
- */
-public interface EchoServerInterface extends Remote {
-	public String processMessage(String hostName, String message) throws java.rmi.RemoteException;
-}
